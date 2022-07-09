1c9cabb1ccda5533eb03703c7ed1357635febf16
SeRozhka <marphiy@gmail.com>
2022-07-09 22:05:02 +0300

    Задание 1

---
+++ point0/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите любое целое число : ");
+int Num = int.Parse(Console.ReadLine());
+int Quad = Num * Num;
+Console.Write("Квадрат числа ");
+Console.Write(Num);
+Console.Write(" = ");
+Console.WriteLine(Quad);
