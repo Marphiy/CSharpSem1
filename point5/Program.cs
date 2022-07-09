287775be2b9b6c0da700b5d4d10ccb22ab177642
SeRozhka <marphiy@gmail.com>
2022-07-09 22:39:47 +0300

    Задание 5

---
+++ point5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Вевдите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Квадрат числа " + number + " = " + Power(number));
+
+int Power(int num1)
+{
+    return num1 * num1;
+}
