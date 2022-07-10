451eccf666603def60e730901c60103d40db181d
SeRozhka <marphiy@gmail.com>
2022-07-10 17:50:12 +0300

    Задание 7

---
+++ point7/Program.cs
@@ -0,0 +1,14 @@
+﻿void point5()
+{
+    Console.WriteLine("Введите любое целое число!");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int start = -number;
+    while (start <= number)
+    {
+        Console.Write(start + " ");
+        start ++;
+    }
+    Console.WriteLine();
+}
+
+point5();
